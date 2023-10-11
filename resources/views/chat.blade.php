c0f4860b2ed3658217f2d94a1d5aac0c225a066e
TuanMinh102 <tuanww012@gmail.com>
2023-10-11 23:12:01 +0700

    chatbox

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -13,55 +13,72 @@
 
     <!-- Favicon  -->
     <link rel="icon" href="img/core-img/favicon.ico">
-
+    <link rel="stylesheet" href="css/chat.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" type="text/css" rel="stylesheet">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/socket.io/2.4.0/socket.io.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/laravel-echo/1.11.0/echo.common.min.js"></script>
     <!-- Core Style CSS -->
    
 
 </head>
 <body>
-<div > <h2 class="title">Chat với hệ thống</h2></div>
-<div id="container">
-    <div><p id="content">cuoc tro chuyen</p></div>
-</div>
-<div id="sendmessage">
-    <input type="text" placeholder="nhập nội dung..." id="input" >
-    <button onclick="send();">Gửi</button>
-</div>
-
-
-<style>
-.title{
-    width: 100%;
-    height:200px;
-    background-color:aqua;
-    text-align:center;
-}
-#container{
-    box-shadow:0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    border:2px solid;
-    width: 100%;
-    height: 300px;
-}
-#container>div{
-    margin-left:10px;
-    width: fit-content;
-    height: 25px;
-    background-color:blue;
-    border-radius:50px;
-    color:white;
-}
-#sendmessage{
-    margin-top:10px;
-    margin-left:30%;
-}
-input{
-    width: 50%;
-    height: 30px;
-}
-button{
-    height: 30px;
-}
-</style>
+<div>
+    <div>
+      <a href="/logout">Logout</a>
+    </div>
+  </div>
+  <div id="app" class="container">
+    <h3 class=" text-center">Messaging | User: Nguyen Minh Tuan</h3>
+    <div class="messaging">
+      <div class="inbox_msg">
+        <div class="inbox_people">
+          <div class="inbox_chat">
+            <div v-for="user in users" class="chat_list">
+              <div class="chat_people">
+                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                <div class="chat_ib">
+                  <h5> minh tuan <span class="chat_date">Dec 25</span></h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="mesgs">
+          <div class="msg_history">
+            <div v-for="message in messages">
+              <div v-if="message.user.id !== id" class="incoming_msg">
+                <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil">
+                </div>
+                <div class="received_msg">
+                  <div class="received_withd_msg">
+                    <p>abcxyz</p>
+                    <span class="time_date"> 11:01 AM | June 9</span>
+                  </div>
+                </div>
+              </div>
+              <div v-else class="outgoing_msg">
+                <div class="sent_msg">
+                  <p>hello chao ban</p>
+                  <span class="time_date"> 11:01 AM | June 9</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="type_msg">
+            <div class="input_msg_write">
+              <input v-model="message" @keyup.enter="sendMessage" type="text" class="write_msg" placeholder="Type a message" />
+              <button @click="sendMessage" class="msg_send_btn" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 <script src="js/chat.js"></script>
 </body>
 </html>
